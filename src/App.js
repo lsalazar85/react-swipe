6e10a4680c1b07f1324d88036bb84bcbf4ec957d
la_salazar85 <luis.salazar@rappi.com>
2019-12-22 02:32:03 -0300

    React hook login function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState, useEffect } from 'react';
+import Swipe from './swipe.js';
+import classNames from 'classnames';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [swiper, setSwiper] = useState(null);
+  const [swiperConfigured, setSwiperConfigured] = useState(false);
+  const [showDeleteButton, setShowDeleteButton] = useState(false);
+  const ref = useCallback(node => {
+    if (node !== null) {
+      setSwiper(new Swipe(node));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (swiper && !swiperConfigured) {
+      swiper.onLeft(() => {
+        setShowDeleteButton(true);
+        return true;
+      });
+      swiper.onRight(() => {
+        setShowDeleteButton(false);
+        return true;
+      });
+      setSwiperConfigured(true);
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-header">
+      <div 
+        className={classNames('slide', showDeleteButton && 'swipeProductOrder', !showDeleteButton && 'swipeProductOrderII')}
+        ref={ref}
+      >
+        Item
+      </div>
+      </section>
     </div>
   );
 }
